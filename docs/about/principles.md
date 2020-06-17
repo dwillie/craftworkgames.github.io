e8545fd5d07e1a883491436a637d50d2d6bffdd4
Lucas Girouard-Stranks <lustranks@gmail.com>
2020-06-17 00:28:14 -0400

    Deploy website - based on 73561ff662c89b93ccbad2d17d0a87b6166654b9

--- docs/about/principles.md
+++
@@ -1,5 +0,0 @@
----
-id: principles
-title: Principles
-sidebar_label: Principles
----
--- sidebars.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  docs: {
-    'MonoGame.Extended': ['about/introduction', 'about/principles'],
-    'Getting Started': ['getting-started/installation', 'getting-started/contributing']
-  },
-};
